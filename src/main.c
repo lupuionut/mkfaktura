a32b6ad4e04ee27196756ade10178296e2e27acf
Ionut Lupu <lupu.ionut@protonmail.ch>
2018-04-11 16:41:59 +0300

    calculations for 2 products

--- src/main.c
+++ src/main.c
@@ -18,42 +18,18 @@ int main(int argc, char *argv[])
     window = GTK_WIDGET(gtk_builder_get_object(builder, "faktura"));
     gtk_builder_connect_signals(builder, NULL);
 
-    input_date =
-        GTK_CALENDAR(gtk_builder_get_object(builder, "input_date"));
-    input_invoice_nr =
-        GTK_ENTRY(gtk_builder_get_object(builder, "input_invoice_nr"));
-    input_currency =
-        GTK_COMBO_BOX_TEXT(gtk_builder_get_object(builder, "input_currency"));
-    input_seller_name =
-        GTK_ENTRY(gtk_builder_get_object(builder, "input_seller_name"));
-    input_seller_business =
-        GTK_ENTRY(gtk_builder_get_object(builder, "input_seller_business"));
-    input_seller_address =
-        GTK_ENTRY(gtk_builder_get_object(builder, "input_seller_address"));
-    input_seller_zip =
-        GTK_ENTRY(gtk_builder_get_object(builder, "input_seller_zip"));
-    input_seller_city =
-        GTK_ENTRY(gtk_builder_get_object(builder, "input_seller_city"));
-    input_seller_country =
-        GTK_COMBO_BOX_TEXT(gtk_builder_get_object(builder, "input_seller_country"));
-    input_seller_nip =
-        GTK_ENTRY(gtk_builder_get_object(builder, "input_seller_nip"));
-    input_buyer_name =
-        GTK_ENTRY(gtk_builder_get_object(builder, "input_buyer_name"));
-    input_buyer_business =
-        GTK_ENTRY(gtk_builder_get_object(builder, "input_buyer_business"));
-    input_buyer_address =
-        GTK_ENTRY(gtk_builder_get_object(builder, "input_buyer_address"));
-    input_buyer_zip =
-        GTK_ENTRY(gtk_builder_get_object(builder, "input_buyer_zip"));
-    input_buyer_city =
-        GTK_ENTRY(gtk_builder_get_object(builder, "input_buyer_city"));
-    input_buyer_country =
-        GTK_COMBO_BOX_TEXT(gtk_builder_get_object(builder, "input_buyer_country"));
-    input_buyer_nip =
-        GTK_ENTRY(gtk_builder_get_object(builder, "input_buyer_nip"));
-    lbl_product1_netto_total =
-        GTK_LABEL(gtk_builder_get_object(builder, "lbl_product1_netto_total"));
+    input_product1_netto =
+        GTK_ENTRY(gtk_builder_get_object(builder, "input_product1_netto"));
+    input_product1_vat_value =
+        GTK_ENTRY(gtk_builder_get_object(builder, "input_product1_vat_value"));
+    input_product1_brutto =
+        GTK_ENTRY(gtk_builder_get_object(builder, "input_product1_brutto"));
+    input_product2_netto =
+        GTK_ENTRY(gtk_builder_get_object(builder, "input_product2_netto"));
+    input_product2_vat_value =
+        GTK_ENTRY(gtk_builder_get_object(builder, "input_product2_vat_value"));
+    input_product2_brutto =
+        GTK_ENTRY(gtk_builder_get_object(builder, "input_product2_brutto"));
 
     g_signal_connect (window, "destroy",
                   G_CALLBACK (destroy), NULL);
@@ -70,6 +46,7 @@ static void destroy(GtkWidget *widget, gpointer data)
     gtk_main_quit();
 }
 
+// PRODUCT 1 CHANGES
 void on_input_product1_net_changed
     (GtkEditable *input_product1_net, GtkComboBoxText *input_product1_qty)
 {
@@ -83,7 +60,7 @@ void on_input_product1_net_changed
     qty = atof(pqt);
     total = netto * qty;
     snprintf(totals,sizeof(totals), "%.2f", total);
-    gtk_label_set_text(lbl_product1_netto_total, totals);
+    gtk_entry_set_text(input_product1_netto, totals);
 }
 
 void on_input_product1_qty_changed
@@ -91,6 +68,95 @@ void on_input_product1_qty_changed
 {
     on_input_product1_net_changed(input_product1_net, input_product1_qty);
 }
+
+void on_input_product1_netto_changed
+    (GtkEditable *input_product1_netto, GtkEntry *input_product1_vat_rate)
+{
+    const char* vat_rate;
+    const char* netto;
+    float vat_rate_float, total_float, vat_float, netto_float;
+    char total[50];
+    char vat[50];
+
+    vat_rate = gtk_entry_get_text(input_product1_vat_rate);
+    netto = gtk_editable_get_chars(input_product1_netto, 0, -1);
+
+    if (strcmp(vat_rate, "-") == 0) {
+        vat_rate = "0";
+    }
+
+    vat_rate_float = atof(vat_rate);
+    netto_float = atof(netto);
+    vat_float = netto_float * vat_rate_float / 100;
+    total_float = netto_float + vat_float;
+    snprintf(vat, sizeof(total), "%.2f", vat_float);
+    snprintf(total, sizeof(total), "%.2f", total_float);
+    gtk_entry_set_text(input_product1_vat_value, vat);
+    gtk_entry_set_text(input_product1_brutto, total);
+}
+
+void on_input_product1_vat_rate_changed
+    (GtkEntry *input_product1_vat_rate, GtkEditable *input_product1_netto)
+{
+    on_input_product1_netto_changed(input_product1_netto, input_product1_vat_rate);
+}
+// PRODUCT1 CHANGES
+
+// PRODUCT 2 CHANGES
+void on_input_product2_net_changed
+    (GtkEditable *input_product2_net, GtkComboBoxText *input_product2_qty)
+{
+    char* pnet;
+    char* pqt;
+    char totals[50];
+    float netto, qty, total;
+    pnet = gtk_editable_get_chars(input_product2_net, 0, -1);
+    netto = atof(pnet);
+    pqt = gtk_combo_box_text_get_active_text(input_product2_qty);
+    qty = atof(pqt);
+    total = netto * qty;
+    snprintf(totals,sizeof(totals), "%.2f", total);
+    gtk_entry_set_text(input_product2_netto, totals);
+}
+
+void on_input_product2_qty_changed
+    (GtkComboBoxText *input_product2_qty, GtkEditable *input_product2_net)
+{
+    on_input_product2_net_changed(input_product2_net, input_product2_qty);
+}
+
+void on_input_product2_netto_changed
+    (GtkEditable *input_product2_netto, GtkEntry *input_product2_vat_rate)
+{
+    const char* vat_rate;
+    const char* netto;
+    float vat_rate_float, total_float, vat_float, netto_float;
+    char total[50];
+    char vat[50];
+
+    vat_rate = gtk_entry_get_text(input_product2_vat_rate);
+    netto = gtk_editable_get_chars(input_product2_netto, 0, -1);
+
+    if (strcmp(vat_rate, "-") == 0) {
+        vat_rate = "0";
+    }
+
+    vat_rate_float = atof(vat_rate);
+    netto_float = atof(netto);
+    vat_float = netto_float * vat_rate_float / 100;
+    total_float = netto_float + vat_float;
+    snprintf(vat, sizeof(total), "%.2f", vat_float);
+    snprintf(total, sizeof(total), "%.2f", total_float);
+    gtk_entry_set_text(input_product2_vat_value, vat);
+    gtk_entry_set_text(input_product2_brutto, total);
+}
+
+void on_input_product2_vat_rate_changed
+    (GtkEntry *input_product2_vat_rate, GtkEditable *input_product2_netto)
+{
+    on_input_product2_netto_changed(input_product2_netto, input_product2_vat_rate);
+}
+// PRODUCT2 CHANGES
 
 void on_btn_print_clicked()
 {
--- src/main.h
+++ src/main.h
@@ -18,15 +18,32 @@ GtkEntry        *input_buyer_nip;
 GtkEntry        *input_product1_name;
 GtkEntry        *input_product1_qty;
 GtkEntry        *input_product1_net;
+GtkEntry        *input_product1_netto;
 GtkEntry        *input_product1_vat_rate;
+GtkEntry        *input_product1_vat_value;
+GtkEntry        *input_product1_brutto;
 GtkEntry        *input_product2_name;
 GtkEntry        *input_product2_qty;
 GtkEntry        *input_product2_net;
+GtkEntry        *input_product2_netto;
 GtkEntry        *input_product2_vat_rate;
-GtkLabel        *lbl_product1_netto_total;
+GtkEntry        *input_product2_vat_value;
+GtkEntry        *input_product2_brutto;
 static void destroy(GtkWidget *widget, gpointer data);
 char* insert_date(char* html);
 void on_input_product1_net_changed
     (GtkEditable *input_product1_net, GtkComboBoxText *input_product1_qty);
 void on_input_product1_qty_changed
     (GtkComboBoxText *input_product1_qty, GtkEditable *input_product1_net);
+void on_input_product1_netto_changed
+    (GtkEditable *input_product1_netto, GtkEntry *input_product1_vat_rate);
+void on_input_product1_vat_rate_changed
+    (GtkEntry *input_product1_vat_rate, GtkEditable *input_product1_netto);
+void on_input_product2_net_changed
+    (GtkEditable *input_product2_net, GtkComboBoxText *input_product2_qty);
+void on_input_product2_qty_changed
+    (GtkComboBoxText *input_product2_qty, GtkEditable *input_product2_net);
+void on_input_product2_netto_changed
+    (GtkEditable *input_product2_netto, GtkEntry *input_product2_vat_rate);
+void on_input_product2_vat_rate_changed
+    (GtkEntry *input_product2_vat_rate, GtkEditable *input_product2_netto);
